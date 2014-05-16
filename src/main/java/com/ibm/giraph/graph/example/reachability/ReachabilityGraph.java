1d067a4c8e9633784da2379d011e8fdbcc7e0778
Yi Lu <luyi0619@outlook.com>
2014-05-16 20:33:55 +0800

    reach graph

--- src/main/java/com/ibm/giraph/graph/example/reachability/ReachabilityGraph.java
+++ src/main/java/com/ibm/giraph/graph/example/reachability/ReachabilityGraph.java
@@ -4,14 +4,20 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+import org.apache.giraph.aggregators.BooleanOrAggregator;
 import org.apache.giraph.graph.GiraphJob;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.apache.giraph.graph.BasicVertex;
+import org.apache.giraph.graph.DefaultMasterCompute;
 import org.apache.giraph.graph.LongLongLongLongVertex;
 import org.apache.giraph.graph.LongLongLongNeighborhood;
+import org.apache.giraph.graph.WorkerContext;
 import org.apache.giraph.graph.partition.HashMasterPartitioner;
 import org.apache.giraph.graph.partition.Partition;
 import org.apache.hadoop.fs.Path;
@@ -37,16 +43,123 @@ public class ReachabilityGraph
 	implements Tool {
 
     private static final Logger LOG = Logger.getLogger(ReachabilityGraph.class);
+    private static String BFS_AGG = "BFS";
     public static int BFS_SOURCE = 0;
     public static int BFS_DEST = 0;
+    private ArrayList<LongLongLongLongVertex> innerVertices = null;
+    
+    
+    public void InitializeTagValue(
+	    Partition<LongWritable, LongWritable, LongWritable, LongWritable> subgraph)
+	    throws IOException {
 
-    SequentialBFS singleBfs = new SequentialBFS();
+	innerVertices = new ArrayList<LongLongLongLongVertex>();
+	Collection<BasicVertex<LongWritable, LongWritable, LongWritable, LongWritable>> vertices = subgraph
+		.getVertices();
 
-    private void updateLabelMapping(
+	Iterator<BasicVertex<LongWritable, LongWritable, LongWritable, LongWritable>> iterator = vertices
+		.iterator();
+	while (iterator.hasNext()) {
+	    LongLongLongLongVertex vertex = (LongLongLongLongVertex) iterator
+		    .next();
+	    vertex.voteToHalt();
+	    long tag = 0;
+	    if (vertex.getVertexIdSimpleType() == ReachabilityGraph.BFS_SOURCE) {
+		tag = 1;
+		innerVertices.add(vertex);
+	    } else if (vertex.getVertexIdSimpleType() == ReachabilityGraph.BFS_DEST) {
+		tag = 2;
+		innerVertices.add(vertex);
+	    }
+	    vertex.setVertexValueSimpleType(tag);
+	}
+
+    }
+
+    private void BFS(
+	    Partition<LongWritable, LongWritable, LongWritable, LongWritable> subgraph) {
+	// TODO Auto-generated method stub
+	BooleanOrAggregator agg  = (BooleanOrAggregator) getAggregator(ReachabilityGraph.BFS_AGG);
+	
+	Queue<LongLongLongLongVertex> q1 = new LinkedList<LongLongLongLongVertex>();
+	Queue<LongLongLongLongVertex> q2 = new LinkedList<LongLongLongLongVertex>();
+
+	Iterator<LongLongLongLongVertex> iterator = innerVertices.iterator();
+
+	while (iterator.hasNext()) {
+	    LongLongLongLongVertex v = iterator.next();
+	    if (v.getVertexValueSimpleType() == 1)
+		q1.add(v);
+	    else if (v.getVertexValueSimpleType() == 2)
+		q2.add(v);
+	}
+
+	while (!q1.isEmpty()) {
+	    LongLongLongLongVertex cur = q1.poll();
+
+	    for (int i = 0; i < cur.getNumOutEdges(); i++) {
+		LongWritable vid = cur.getEdgeID(i);
+		long value = cur.getSimpleEdgeValue(i);
+
+		if (value == 1)
+		    continue; // only out edges
+
+		LongLongLongLongVertex nvertex = (LongLongLongLongVertex) subgraph
+			.getVertex(vid);
+
+		if (nvertex == null) {
+		    cur.sendMsg(vid, cur.getVertexValue());
+		} else {
+		    long tag = nvertex.getVertexValueSimpleType();
+		    if ((tag | 1) != tag) {
+			tag |= 1;
+			nvertex.setVertexValueSimpleType(tag);
+			if (tag == 3) {
+			    agg.aggregate(true);
+			} else
+			    q1.add(nvertex);
+		    }
+		}
+	    }
+	}
+
+	while (!q2.isEmpty()) {
+	    LongLongLongLongVertex cur = q2.poll();
+
+	    for (int i = 0; i < cur.getNumOutEdges(); i++) {
+		LongWritable vid = cur.getEdgeID(i);
+		long value = cur.getSimpleEdgeValue(i);
+
+		if (value == 0)
+		    continue; // only in edges
+
+		LongLongLongLongVertex nvertex = (LongLongLongLongVertex) subgraph
+			.getVertex(vid);
+
+		if (nvertex == null) {
+		    cur.sendMsg(vid, cur.getVertexValue());
+		} else {
+		    long tag = nvertex.getVertexValueSimpleType();
+		    if ((tag | 2) != tag) {
+			tag |= 2;
+			nvertex.setVertexValueSimpleType(tag);
+			if (tag == 3) {
+			    agg.aggregate(true);
+			} else
+			    q2.add(nvertex);
+		    }
+		}
+	    }
+	}
+
+    }
+
+    private void updateLabel(
 	    Partition<LongWritable, LongWritable, LongWritable, LongWritable> subgraph) {
 	Iterator<BasicVertex<LongWritable, LongWritable, LongWritable, LongWritable>> iterator = subgraph
 		.getVertices().iterator();
 
+	innerVertices = new ArrayList<LongLongLongLongVertex>();
 	// process all the messages to update label mappings
 	while (iterator.hasNext()) {
 	    LongLongLongLongVertex vertex = (LongLongLongLongVertex) iterator
@@ -64,14 +177,7 @@ public class ReachabilityGraph
 	    if (newtag == oldtag)
 		continue;
 	    vertex.setVertexValueSimpleType(newtag);
-	    for (long neighbor : vertex.getNeighborsSimpleType()) {
-		LongLongLongLongVertex nVertex = (LongLongLongLongVertex) subgraph
-			.getVertex(new LongWritable(neighbor));
-		if (nVertex == null) {
-		    vertex.sendMsg(new LongWritable(neighbor),
-			    new LongWritable(newtag));
-		}
-	    }
+	    innerVertices.add(vertex);
 	}
     }
 
@@ -80,14 +186,65 @@ public class ReachabilityGraph
 	    final Partition<LongWritable, LongWritable, LongWritable, LongWritable> subgraph)
 	    throws IOException {
 
+	innerVertices = null;
+	BooleanOrAggregator agg  = (BooleanOrAggregator) getAggregator(ReachabilityGraph.BFS_AGG);
+	
+	if(agg.getAggregatedValue().get())
+	{
+		return;
+	}
+
 	if (this.getSuperstep() == 0) {
-	    singleBfs.LabelSourceAndDest(subgraph);
+	    InitializeTagValue(subgraph);
+	    BFS(subgraph);
 	} else {
-	    updateLabelMapping(subgraph);
+	    updateLabel(subgraph);
+	    BFS(subgraph);
 	}
 
     }
+	public static class ReachabilityWorkerContext extends WorkerContext {
 
+		private static boolean FINAL_BFS;
+
+		public static boolean getFinalStar() {
+			return FINAL_BFS;
+		}
+
+		@Override
+		public void preApplication() throws InstantiationException,
+				IllegalAccessException {
+		}
+
+		@Override
+		public void postApplication() {
+
+			BooleanOrAggregator agg  = (BooleanOrAggregator) getAggregator(ReachabilityGraph.BFS_AGG);
+			FINAL_BFS = agg.getAggregatedValue().get();
+		}
+
+		@Override
+		public void preSuperstep() {
+
+		}
+
+		@Override
+		public void postSuperstep() {
+		}
+	}
+
+	/**
+	 * Master compute associated with {@link SimplePageRankComputation}. It
+	 * registers required aggregators.
+	 */
+	public static class ReachabilityMasterCompute extends
+			DefaultMasterCompute {
+		@Override
+		public void initialize() throws InstantiationException,
+				IllegalAccessException {
+			registerAggregator(BFS_AGG, BooleanOrAggregator.class);
+		}
+	}
     @Override
     public int run(String[] args) throws Exception {
 	if (args.length != 5) {
